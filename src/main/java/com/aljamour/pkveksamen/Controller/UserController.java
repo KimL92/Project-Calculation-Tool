8ef444941cc498c7702d930553c4826397a995d0
Aden0020 <aden0020@hotmail.com>
2025-11-19 14:21:21 +0100

    tilføjede kodning

--- src/main/java/com/aljamour/pkveksamen/Controller/UserController.java
+++ src/main/java/com/aljamour/pkveksamen/Controller/UserController.java
@@ -1,5 +1,23 @@
 package com.aljamour.pkveksamen.Controller;
 
+import com.aljamour.pkveksamen.Model.UserModel;
+import com.aljamour.pkveksamen.Service.UserService;
+import org.apache.catalina.User;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class UserController {
-//gdff
 }
+
+
+//    private UserService userService;
+//
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
+//    @GetMapping("/create-user")
+//    public String createUser(Model model) {
+//        model.addAttribute("user", new UserModel());
+//        return "register-user";
+//    }
+//}
--- src/main/java/com/aljamour/pkveksamen/Repository/UserRepository.java
+++ src/main/java/com/aljamour/pkveksamen/Repository/UserRepository.java
@@ -2,3 +2,17 @@ package com.aljamour.pkveksamen.Repository;
 
 public class UserRepository {
 }
+
+
+//    private final JdbcTemplate jdbcTemplate;
+//
+//    public UserRepository(JdbcTemplate jdbcTemplate) {
+//        this.jdbcTemplate = jdbcTemplate;
+//    }
+//
+//    public void createUser(String userName, String email, String userPassword, String role) {
+//        jdbcTemplate.update("INSERT INTO user(username, email, user_password) VALUES (?, ? , ?) ",
+//                userName, email, userPassword, role);
+//        System.out.println(userName + email + userPassword);
+//    }
+//}
